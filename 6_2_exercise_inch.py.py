85fa6bf9abbd81a2671b300663ad2765dc8b3bc7
Suzan Laura Ergöz <susan.ergoez@gmx.de>
2024-06-25 19:48:30 +0200

    Delete 6_2_exercise_inch.py.py

--- 6_2_exercise_inch.py.py
+++
@@ -1,9 +0,0 @@
-Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> inc=2.54
->>> 5*inc
-12.7
->>> 20*inc
-50.8
->>> 92.7*inc
-235.458
