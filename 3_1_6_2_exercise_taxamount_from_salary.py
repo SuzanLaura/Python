13c0baad229efddbbc0b64f1326018d2c27c1090
Suzan Laura Ergöz <susan.ergoez@gmx.de>
2024-07-02 16:49:17 +0200

    Add files via upload

---
+++ 3_1_6_2_exercise_taxamount_from_salary.py
@@ -0,0 +1,13 @@
+#taxamaount=18%
+
+print("Please insert your monthly salary in euro:")
+
+s=input()
+snumber=int(s)
+
+print("Yourt input:",s)
+
+stax=snumber*0.18
+
+print("Your tax amount are", stax,"euro")
+
