77521ea2f3107b71fa15f31c7f903470c3218cf7
Suzan Laura Ergöz <susan.ergoez@gmx.de>
2024-07-04 13:48:50 +0200

    Add files via upload

---
+++ 3_2_5_multiple_branches.py
@@ -0,0 +1,13 @@
+import random
+random.seed
+x = random.randint(-5,5)
+print("x:",x)
+
+if x > 0:
+    print("x is positive")
+
+elif x < 0:
+    print("x is negative")
+
+else:
+    print("x is 0")
