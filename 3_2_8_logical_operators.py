b8b96aa16f951bad755ff810568042c683e0a5e4
Suzan Laura Ergöz <susan.ergoez@gmx.de>
2024-07-04 17:53:13 +0200

    Add files via upload

---
+++ 3_2_8_logical_operators.py
@@ -0,0 +1,14 @@
+x = 12
+y = 15
+z = 20
+print(f"x:{x}, y:{y}, z:{z}")
+
+if x < y and x < z:
+    print("x is the smallest number")
+
+if y > x or y > z:
+    print("y is not the smallest number")
+
+if not y < x:
+    print("y is not smaller than x")
+    
